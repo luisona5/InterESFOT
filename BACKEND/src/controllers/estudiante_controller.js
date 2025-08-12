93f2843f1a0418d775034613e5a8463b2d7af7cc
luisona5 <150484099+luisona5@users.noreply.github.com>
2025-08-12 09:51:20 -0500

    Update estudiante_controller.js

--- BACKEND/src/controllers/estudiante_controller.js
+++ BACKEND/src/controllers/estudiante_controller.js
@@ -80,10 +80,10 @@ const detalleEstudiante = async(req,res)=>{
     if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el administrador ${id}`});
     const estudiante = await Estudiante.findById(id).select("-createdAt -updatedAt -__v").populate('administrador','_id nombre apellido')
 
-    const deportes = await deportes.find().where('estudiante').equals(id)
+    const deportes = await Deporte.find().where('estudiante').equals(id)
     res.status(200).json({
         estudiante,
-        deportes
+        Deporte
     })
 }
 
